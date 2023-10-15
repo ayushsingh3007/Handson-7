eefe3e70c3f4c3f2e0b55a714139388e7245bcb2
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-15 12:06:35 -0700

    Update Home.js

--- src/Redu/CompoUI/Home.js
+++ src/Redu/CompoUI/Home.js
@@ -2,10 +2,10 @@ import React from "react";
  
 const Home=()=>{
     return(
-        <>
+        <div>
         <h1>Welcome on the Home page </h1>
         
-        </>
+        </div>
     )
 }
 export {Home}
