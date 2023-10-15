2ab4e5e61dc82ed4a3d95ffb4e39b0bf994b9d02
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-15 12:07:07 -0700

    Update Contact.js

--- src/Redu/CompoUI/Contact.js
+++ src/Redu/CompoUI/Contact.js
@@ -2,10 +2,10 @@ import React from "react";
  
 const Contact=()=>{
     return(
-        <>
+        <div>
         <h1>Welcome on the Contact us page </h1>
         
-        </>
+        </div>
     )
 }
 export {Contact};
