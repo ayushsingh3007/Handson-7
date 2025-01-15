2be27e7f48290b9c4655e0d57a0a4b0128d020b5
Ayush singh <131241505+ayushsingh3007@users.noreply.github.com>
2025-01-15 11:26:10 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import { RouteCompo } from './Redu/CompoUI/RouteCompo';
+import {Link} from "BrowserRouter"
 
 
 function App() {
    return (
     <div className="App">
+       <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/student">Student</Link>
+                <Link className="nav-link" to="contact">Contact</Link>
 <RouteCompo/>
 
     </div>
